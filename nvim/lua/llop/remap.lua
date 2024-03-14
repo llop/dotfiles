054dc03d5b864e1cc442b78556733d1d506829d1
Albert Lobo <llop@system76-pc.localdomain>
2024-03-14 01:09:36 -0700

    Update nvim keybindings

--- nvim/lua/llop/remap.lua
+++ nvim/lua/llop/remap.lua
@@ -1,13 +1,11 @@
 vim.g.mapleader = ' '
 
 -- NvimTree keybindings
-vim.keymap.set('n', '<leader>x', ':NvimTreeFocus<CR>')
+vim.keymap.set('n', '<leader>i', ':NvimTreeFocus<CR>')
+vim.keymap.set('n', '<leader>o', ':NvimTreeToggle<CR>')
 
 -- BarBar keybindings
 vim.keymap.set('n', '<A-,>', ':BufferPrevious<CR>')
 vim.keymap.set('n', '<A-.>', ':BufferNext<CR>')
 vim.keymap.set('n', '<A-c>', ':BufferClose!<CR>')
-
--- Terminal keybindings
-vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
 
